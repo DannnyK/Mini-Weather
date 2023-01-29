1c1f4722dd1bb1cd218b9a189047f311835629af
Daniel Kruger <danielkruger98@gmail.com>
2023-01-29 19:51:41 +0200

    changed secret handling

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 require("dotenv").config();
 
 const test = process.env.TEST;
-const weatherApiKey = process.env.WEATHER_API_KEY;
+const weatherApiKey =
+	process.env.WEATHER_API_KEY || "ac4316501e6a477a96581957232701";
 
 router
 	.get("/weather", (req, res) => {
