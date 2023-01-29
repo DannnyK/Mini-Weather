f5cfd970aa9c037451c2277fb6190f4f87676137
Daniel Kruger <danielkruger98@gmail.com>
2023-01-29 23:27:47 +0200

    fixed env handling

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,8 +3,7 @@ const router = express.Router();
 require("dotenv").config();
 
 const test = process.env.TEST;
-const weatherApiKey =
-	process.env.WEATHER_API_KEY || "ac4316501e6a477a96581957232701";
+const weatherApiKey = process.env.WEATHER_API_KEY;
 
 router
 	.get("/weather", (req, res) => {
